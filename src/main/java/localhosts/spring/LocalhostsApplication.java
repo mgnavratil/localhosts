9d6e014d87f73e715149a4e5991631cf3f4db346
Reglius <pawc3@cox.net>
2020-10-16 19:57:26 -0500

    initial all file commit for springboot applicaiton

---
+++ src/main/java/localhosts/spring/LocalhostsApplication.java
@@ -0,0 +1,13 @@
+package localhosts.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalhostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalhostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/localhosts/spring/LocalhostsApplicationTests.java
@@ -0,0 +1,13 @@
+package localhosts.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalhostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
