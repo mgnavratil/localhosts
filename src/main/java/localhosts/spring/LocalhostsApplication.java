a171926d627c249e45baff8a0c95510ec26c8d34
Reglius <pawc3@cox.net>
2020-10-28 14:10:26 -0500

    fixed the db not connecting on initial startup

--- src/main/java/localhosts/spring/LocalhostsApplication.java
+++ src/main/java/localhosts/spring/LocalhostsApplication.java
@@ -1,13 +1,32 @@
 package localhosts.spring;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class LocalhostsApplication {
+public class LocalhostsApplication implements CommandLineRunner{
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	private static final Logger log = LoggerFactory.getLogger(LocalhostsApplication.class);
+	  
 	public static void main(String[] args) {
 		SpringApplication.run(LocalhostsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... strings) throws Exception {
+
+	    log.info("running");
+	  }
 }
